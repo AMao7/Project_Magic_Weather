cb28da75d564428f9cc06905bed07af302e947e2
AMao7 <abdimalikmao7@gmail.com>
2020-02-07 11:17:38 +0000

    first commit of the magic weather project

---
+++ Magic_Weather.py
@@ -0,0 +1,13 @@
+weather = 'rainy'
+
+if weather == 'rainy and stormy':
+    print('take umbrella')
+    print("I'm still in the block of code as well :D ")
+elif weather == 'stormy':
+    print("take raincoat")
+
+elif ('stormy' in weather) and ('rainy' in weather):
+    print("stay at home g")
+else:
+    print('take sunglasses')
+
---
+++ README.md
@@ -0,0 +1,5 @@
+# Project 3: Magic Weather
+
+### Learning Objectives: To provide advice for user on what actions to take regarding the weather
+
+#### Utilised: Control flow, functions, variables
